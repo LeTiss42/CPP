67caa2177f5dd1c2f6d3f097ad67b0bf37772673
Mathis Ravera <mravera@student.42lausanne.ch>
2023-05-29 11:50:36 +0200

    only throw exceptions, no catch in fonction

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mravera <mravera@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 11:55:18 by mravera           #+#    #+#             */
-/*   Updated: 2023/05/28 19:16:09 by mravera          ###   ########.fr       */
+/*   Updated: 2023/05/29 11:22:21 by mravera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,38 @@
 int	main( void ) {
 
 	Bureaucrat oui;
+	std::cout << "name : " << oui.getName() << "\ngrade : " << oui.getGrade() << std::endl << std::endl;
 	try {
-		Bureaucrat non("jannot", 200);
+		Bureaucrat MrLow("MrLow", 200);
 	}
 	catch (Bureaucrat::GradeTooLowException& l) {
-		std::cout << l.what() << std::endl;
-		return 0;
+		std::cout << l.what() << "MrLow constructor" << std::endl << std::endl;
 	}
+	
+	try {
+		Bureaucrat MrHigh("MrHigh", 0);
+	}
+	catch (Bureaucrat::GradeTooHighException& l) {
+		std::cout << l.what() << "MrHigh constructor" << std::endl << std::endl;
+	}
+
+	Bureaucrat	MrIncHigh("MrIncHigh", 1);
+	std::cout << MrIncHigh;
+	try {
+		MrIncHigh.incGrade();
+	}
+	catch (Bureaucrat::GradeTooHighException& l) {
+		std::cout << l.what() << "MrIncHigh" << std::endl << std::endl;
+	}
+
+	Bureaucrat	MrIncLow("MrIncLow", 150);
+	std::cout << MrIncLow;
+	try {
+		MrIncLow.decGrade();
+	}
+	catch (Bureaucrat::GradeTooLowException& l) {
+		std::cout << l.what() << "MrIncLow" << std::endl << std::endl;
+	}
+
 	return 0;
 }
