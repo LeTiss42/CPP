d1040c26e2b4cdb8eddeae87111b7be79afa6226
Mathis Ravera <mravera@student.42lausanne.ch>
2023-06-13 16:52:04 +0200

    dataSet parsed as map

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -6,19 +6,21 @@
 /*   By: mravera <mravera@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 17:07:27 by mravera           #+#    #+#             */
-/*   Updated: 2023/06/13 13:20:05 by mravera          ###   ########.fr       */
+/*   Updated: 2023/06/13 16:50:40 by mravera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-BitChange::BitChange(void) : _filename("data.csv") {
+//Default constructor take gives "data.csv" as default file name for the dataset used by the program.
+BitChange::BitChange(void) : _dataSetName("data.csv") {
 
 	std::cout << "+ BitChange default constructor called" << std::endl;
 	return ;
 }
 
-BitChange::BitChange(std::string filename) : _filename(filename) {
+//Parametric constructor allows one to choose the dataset used by the program.
+BitChange::BitChange(std::string _dataSetName) : _dataSetName(_dataSetName) {
 
 	std::cout << "+ BitChange parametric constructor called" << std::endl;
 	return ;
@@ -32,7 +34,7 @@ BitChange::BitChange(BitChange const & src) {
 
 BitChange &	BitChange::operator=(BitChange const & rhs) {
 
-	this->_data = rhs._data;
+	this->_map = rhs._map;
 	return *this;
 }
 
@@ -42,24 +44,63 @@ BitChange::~BitChange(void) {
 	return ;
 }
 
+void	BitChange::dispData(void) {
+
+	std::cout << "---data---" << std::endl;
+	for (BitChange::iter it = _map.begin(); it != _map.end(); it++)
+		std::cout << it->first << " - " << it->second << std::endl;
+	return ;
+}
+
+//readCsv take the file name stored in the BitChange class and stores its content in the map called _map.
 int	BitChange::readCsv(void) {
 
 	std::fstream	myfile;
 	std::string		buff;
-	std::string		dateSet = "0123456789-";
-	std::string		valueSet = "0123456789.";
+	std::string		dateSet = "0123456789-";	//Set of all characters appearing in a date in the csv
+	std::string		valueSet = "0123456789.";	//Set of all characters appearing in a value in the csv
 
-	myfile.open(this->_filename, std::ios::in);
-	if (myfile.is_open() && std::getline(myfile, buff)) {
+	myfile.open(this->_dataSetName, std::ios::in);
+	if (myfile.is_open()) {	//Open the csv file and check success.
+		std::getline(myfile, buff);	//Get the first line to avoid parsing it.
 		while(std::getline(myfile, buff))
-			this->_data[std::string(buff, 0, buff.find_first_not_of(dateSet))] = std::string(buff, (buff.find_last_not_of(valueSet) + 1), std::string::npos);
+		 	//The csv file should be correct, we first place the substring from beginning to last character that does not appear in dateSet in a map.
+			//Place the substring from the last character that does not appear in the valueSet to last character in the map as value for previous date.
+			try {
+				this->_map[std::string(buff, 0, buff.find_first_not_of(dateSet))] = std::string(buff, (buff.find_last_not_of(valueSet) + 1), std::string::npos);
+			}
+			catch (const std::exception &exc) {
+				std::cout << "Caught exception \"" << exc.what() << "\"" << std::endl;
+			}
 	}
 	else {
 		std::cout << "Error in function readCsv() => " << strerror(errno) << std::endl;
 		return 1;
 	}
-	std::cout << std::endl << "---data---" << std::endl;
-	for (std::map<std::string, std::string>::iterator it = _data.begin(); it != _data.end(); it++)
-		std::cout << it->first << " - " << it->second << std::endl;
+	myfile.close();
+	return 0;
+}
+
+//main function of the program
+int	BitChange::exchange(std::string filename) {
+
+	std::fstream	myfile;
+	std::string		buff;
+
+	myfile.open(filename, std::ios::in);
+	if (!myfile.is_open()) {
+		std::cout << "Error in function exchange() => " << strerror(errno) << std::endl;
+		return 1;
+	}
+	std::cout << "----- " << filename << " has been successfully opened. -----" << std::endl;
+	if (this->readCsv()) {
+		std::cout << "Error while parsing " << this->_dataSetName << " as dataset." << std::endl;
+		return 1;
+	}
+	std::cout << "----- " << this->_dataSetName << " has been successfully parsed. -----" << std::endl;
+	std::getline(myfile, buff); //skip first line to avoid working on the columns' name
+	while (std::getline(myfile, buff)) {
+		std::cout << buff << std::endl;
+	}
 	return 0;
 }
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: mravera <mravera@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 16:07:20 by mravera           #+#    #+#             */
-/*   Updated: 2023/06/13 13:04:25 by mravera          ###   ########.fr       */
+/*   Updated: 2023/06/13 16:50:51 by mravera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,22 @@ class BitChange {
 public:
 
 	BitChange(void);
-	BitChange(std::string filename);
+	BitChange(std::string _dataSet);
 	BitChange(BitChange const & src);
 	BitChange & operator=(BitChange const & rhs);
 	~BitChange(void);
 
-	int	readCsv(void);
+	int		exchange(std::string filename);
+	void	dispData(void);
 
 	typedef std::map<std::string, std::string>::iterator	iter;
 
 private:
 
-	std::string						_filename;
-	std::map<std::string, std::string>	_data;
+	std::string						_dataSetName;
+	std::map<std::string, std::string>	_map;
+
+	int		readCsv(void);
 
 };
 
